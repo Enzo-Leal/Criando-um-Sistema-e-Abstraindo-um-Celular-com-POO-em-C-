ddc8c7e9acdfa47260a62b203676e5b4b3cb3e6a
Enzo Leal De Miranda <69826715+Enzo-Leal@users.noreply.github.com>
2024-06-01 19:22:12 -0300

    Add files via upload

---
+++ desafio telefone/Program.cs
@@ -0,0 +1,16 @@
+﻿using desafio_telefone.models;
+
+
+Console.Clear();
+
+Console.WriteLine("Smartphone NOKIA");
+Smartphone nokia = new Samsung(numero: "123456", modelo: "NOKIA C20", imei: "1212121212", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Iphone 14", imei: "2323232323", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
---
+++ desafio telefone/models/iphone.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio_telefone.models
+{
+   
+    public class Iphone : Smartphone
+    {
+        
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Iphone");
+        }
+    }
+}
